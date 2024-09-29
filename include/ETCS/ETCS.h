c1101c6ac62a8e9cec4037afa92ad8b2c580f13e
Zhile Zhu <zhuzhile08@gmail.com>
2024-09-29 18:59:20 +0200

    Remove init functions

--- include/ETCS/ETCS.h
+++ include/ETCS/ETCS.h
@@ -2,7 +2,7 @@
  * @file ETCS.h
  * @author Zhile Zhu (zhuzhile08@gmail.com)
  * 
- * @brief Entity Component System API
+ * @brief ETCS library header
  * 
  * @date 2024-01-31
  * 
@@ -15,26 +15,7 @@
 
 #include "Entity.h"
 #include "Component.h"
-#include "System.h"
 #include "World.h"
+#include "EntityQuery.h"
+#include "EntityRange.h"
 #include "Components/Transform.h"
-
-namespace etcs {
-
-void init();
-
-void quit();
-
-
-[[nodiscard]] inline auto insertEntity(string_view_t name = "") {
-	return detail::globalWorld->insertEntity(name);
-}
-[[nodiscard]] inline auto insertEntity(string_view_t name, Entity parent) {
-	return detail::globalWorld->insertEntity(name, parent);
-}
-
-template <class Callable, class... Types> [[nodiscard]] inline auto insertSystem(Callable&& callable) {
-	return detail::globalWorld->insertSystem<Callable, Types...>(std::forward<Callable>(callable));
-}
-
-} // namespace etcs
